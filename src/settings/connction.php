564043eb39bf7d8acefa8ab3a5f0792f6535d2ed
Luís Gustavo Alves Bezerra <oluisgustavoalves@gmail.com>
2024-09-14 15:30:53 -0300

    Configuração Inicial do Banco de Dados
    - Adicionado o arquivo .env para armazenar variáveis privadas.
    - Configurado o acesso ao banco de dados MySQL usando a biblioteca `phpdotenv` para ler variáveis de ambiente.
    - Implementado o código de conexão com o banco de dados MySQL, verificando a conexão e lidando com erros.
    - Atualizado o arquivo .gitignore para garantir que o arquivo .env não seja incluído no controle de versão.

---
+++ src/settings/connction.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'C:/xampp/htdocs/expressproject/vendor/autoload.php'; // Caminho absoluto
+
+use Dotenv\Dotenv;
+
+// Carregar o arquivo .env
+$dotenv = Dotenv::createImmutable('C:/xampp/htdocs/expressproject'); // Caminho absoluto
+$dotenv->load();
+
+// Acessar as variáveis de ambiente
+$servidor = $_ENV['DB_HOST'];
+$banco = $_ENV['DB_NAME'];
+$usuario = $_ENV['DB_USER'];
+$porta = $_ENV['DB_PORT'];
+$senha = $_ENV['DB_PASS'];
+
+try {
+    // Criar a conexão com o banco de dados
+    $conn = new mysqli($servidor, $usuario, $senha, $banco, $porta);
+
+    // Verificar se a conexão foi bem-sucedida
+    if ($conn->connect_errno) {
+        throw new Exception("Conexão falhou: (" . $conn->connect_errno . ") " . $conn->connect_error);
+    } else {
+        echo "Conectado com sucesso!";
+    }
+
+    // Fechar a conexão quando terminar
+    $conn->close();
+} catch (Exception $e) {
+    echo "Erro: " . $e->getMessage();
+}
+?>
