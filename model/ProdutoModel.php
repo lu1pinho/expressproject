ef2d8f18dcec9c64020fa6e5b233c61a55971efb
dallylademoraes <moraesdallyla6@gmail.com>
2024-11-26 21:44:03 -0300

    testes atualizar vendedor e pagina principal

--- model/ProdutoModel.php
+++ model/ProdutoModel.php
@@ -1,6 +1,7 @@
 <?php
+namespace Model;
 // Inclui a conexão com o banco de dados
-include '../settings/connection.php';
+//include '../settings/connection.php'; // Não é necessário incluir o arquivo de conexão de teste
 
 class ProdutoModel {
 
